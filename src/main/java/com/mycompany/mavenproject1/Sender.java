3aa2414698f9ea7ddb6e1ecd2c60288f8a0b3432
Ali Ragab <92127425+Ali313SS@users.noreply.github.com>
2024-12-20 14:33:17 +0200

    Update Sender

--- src/main/java/com/mycompany/mavenproject1/Sender.java
+++ src/main/java/com/mycompany/mavenproject1/Sender.java
@@ -1,12 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
+
 package com.mycompany.mavenproject1;
 
 /**
  *
- * @author 20101
+ * @author AliRagab313
  */
 public interface Sender {
     public void sendMessage(String receiverUserName,String Content);     
